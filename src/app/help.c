722ca5eb567b8ceb1a9d08a6472c65b8841cfb4f
patrick <patrick.yates@epixsystems.co.uk>
2024-01-31 11:46:49 +0000

    changing help to use getFile and updating the help file

--- src/app/help.c
+++ src/app/help.c
@@ -1,13 +1,9 @@
 #include "help.h"
 #include "../files/fileLib.h"
-#include <string.h>
 #include <stdlib.h>
 
 void getHelp(){
-    char* file = malloc(strlen(getenv("HOME")) + strlen(HELP_FILE) + 1);
-    strcpy(file, getenv("HOME"));
-    strcat(file, HELP_FILE);
-    
+    char* file = getFile(HELP_FILE); 
     printListFile(file, 0); 
     free(file);
 }
